0ca404ca7e8fa00a254eb9f1a120cdabad8015b5
Catherine Cui <ccui@college.harvard.edu>
2022-04-19 00:29:15 -0400

    deploy testing

--- web22/settings.py
+++ web22/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/visuals/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'visuals/static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
